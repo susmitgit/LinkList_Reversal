7303e24de347ded398dc25acf7e7392e152b7b40
Susmit Basu <bosesusmit2006@gmail.com>
2021-04-13 10:11:44 +0000

    Handle for 0 value

--- main.py
+++ main.py
@@ -38,14 +38,28 @@ class Solution:
     return final_val
 
   def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-    converted_stack = self.convert_stack(l1)
+    if not l1 or l1.val == 0:
+      l1_val = 0
+    else:
+      l1_val = self.reverse_stack_value(self.convert_stack(l1))
+    if not l2 or l2.val == 0:
+      l2_val = 0
+    else:
+      l2_val = self.reverse_stack_value(self.convert_stack(l2))
     
-    l1_val = self.reverse_stack_value(self.convert_stack(l1))
-    l2_val = self.reverse_stack_value(self.convert_stack(l2))
-    link_list = self.make_linkedlist(str(int(l1_val) + int(l2_val)))
+    add = str(int(l1_val) + int(l2_val))
+    print('add', add)
+    if add == 0:
+      link_list = ListNode()
+    else:
+      link_list = self.make_linkedlist(str(int(l1_val) + int(l2_val)))
     self.convert_stack(link_list)
     return link_list
    
 head1 = ListNode(2, ListNode(4, ListNode(3)))
 head2 = ListNode(5, ListNode(6, ListNode(4)))
+
+head1 = ListNode(0)
+head2 = ListNode(0)
+
 Solution().addTwoNumbers(head1,head2)
